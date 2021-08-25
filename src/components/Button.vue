ddbf00d6e1c35d457aefadd4b934446caf9c55d2
code-tieumomo <code.tieumomo@gmail.com>
2021-08-26 00:44:56 +0700

    Init project

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="btn" :style="{backgroundColor: color}" @click="onClick()">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String
+  },
+  methods: {
+    onClick() {
+      console.log("click");
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
