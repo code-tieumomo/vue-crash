deb19214a5aab7e3b133d4b4d2dbee1ebeafd87b
code-tieumomo <code.tieumomo@gmail.com>
2021-09-04 02:04:26 +0700

    Update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="root">
-    <h1>About</h1>
-    <h3>Version 1.0.0</h3>
-    <router-link to="/">Back</router-link>
+    <router-link to="/"><i class="fa fa-angle-left"></i> Back</router-link>
+    <h1>About Us</h1>
+    <h2>
+      Just a simple note book <br />
+      (or exactly a todos list)
+    </h2>
+
+    <h4 class="m-t-2"># CHANGE NOTES</h4>
+    <div>
+      <span class="versions">@1.0.0</span> <br />
+      Release date: 04/09/2021 <br />
+      <p class="version-details">
+        <ul>
+          <li>Basic features (view, add, toggle status, remove todos).</li>
+          <li>Store todos in database.</li>
+          <li>Realtime update.</li>
+        </ul>
+      </p>
+    </div>
   </div>
 </template>
 
@@ -13,5 +29,19 @@ export default {
 </script>
 
 <style scoped>
-
+.root {
+  padding-top: 1rem;
+}
+.m-t-2 {
+  margin-top: 2rem;
+}
+.versions {
+  color: brown;
+}
+ul {
+  list-style-type: circle;
+}
+li{
+  margin-inline-start: 20px;
+}
 </style>
